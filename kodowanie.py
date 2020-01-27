9b29eb4648e0e9df2b844a218adf132077c8d302
unknown <mateusz.zietara8721@gmail.com>
2020-01-27 16:03:31 +0100

    m

--- kodowanie.py
+++ kodowanie.py
@@ -1,20 +1,18 @@
 
 
-a = 'TOto1212'
+p = 'Wiktoria Płaszczyk'
 
-l2 = [ord(c) for c in a]
+wez_p = [ord(c) for c in p]
 
-for i in range(len(l2)):
-    if l2[i] < 10:
-        l2[i] = str(l2[i])
-        l2[i] = '00' + l2[i]
-    if l2[i] <100:
-        l2[i] = str(l2[i])
-        l2[i] = '0' + l2[i]
+for i in range(len(wez_p)):
+    if wez_p[i] < 10:
+        wez_p[i] = str(wez_p[i])
+        wez_p[i] = '00' + wez_p[i]
+    if wez_p[i] <100:
+        wez_p[i] = str(wez_p[i])
+        wez_p[i] = '0' + wez_p[i]
 
-print(''.join(map(str, l2)))
-
-
+print(''.join(map(str, wez_p)))
 
 
 
