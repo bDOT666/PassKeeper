c807f669964180cf0ffa9b47fb56df066452c222
Mateusz Ziętara <mateusz.zietara8721@gmail.com>
2020-02-15 17:57:20 +0100

    cos

--- kodowanie.py
+++ kodowanie.py
@@ -13,45 +13,17 @@ Ascii na liczbe
 for i in range(len(wez_p)):
     if wez_p[i] < 10:
         wez_p[i] = str(wez_p[i])
-        wez_p[i] = '00' + wez_p[i]
+        wez_p[i] = '00' + str(wez_p[i])
     if wez_p[i] <100:
         wez_p[i] = str(wez_p[i])
-        wez_p[i] = '0' + wez_p[i]
+        wez_p[i] = '0' + str(wez_p[i])
 
 a = ''.join(map(str, wez_p))
 print(a)
 
-"""
-KODOWANIE
-"""
-float(a)
-
-a1 = a * 13412
-print(a1)
-
-
-
-
-
-
-
-"""
-ODKODOWANIE
-"""
-
-
-
-
-a2 = 1/13412
-print(a2)
-
-"""
-liczba na Ascii
-"""
 
 b = []
 for i in range(0, len(a), 3):
-    c = a[i:i + 3]
     b.append(int(a[i:i + 3]))
 print(b)
 
@@ -59,13 +31,49 @@ daj_p = ''.join(chr(i) for i in b)
 print(daj_p)
 
 
+class KodowanieOdkodowania:
+    key = 0
+
+    def __init__(self):
+        self.wez_p = []
+        self.daj_p = []
+
+    def tekst_na_askii(self, tekst):
+        self.wez_p = [ord(c) for c in tekst]
+        for i in range(len(self.wez_p)):
+            if wez_p[i] < 10:
+                self.wez_p[i] = str(self.wez_p[i])
+                self.wez_p[i] = '00' + str(self.wez_p[i])
+            if self.wez_p[i] < 100:
+                self.wez_p[i] = str(wez_p[i])
+                self.wez_p[i] = '0' + str(self.wez_p[i])
+
+        return int(''.join(map(str, self.wez_p)))
+
+    def aski_na_tekst(self, liczba):
+        for i in range(0, len(liczba), 3):
+            self.daj_p.append(int(liczba[i:i + 3]))
+
+        return ''.join(chr(i) for i in b)
 
 
-import tkinter as tk
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 class Yes:
     a = 1
+
     def __init__(self):
         pass
 
@@ -75,21 +83,24 @@ class Yes:
         else:
             print("No, but yes")
 
+
 class No(Yes):
 
     def no(self):
-        if Yes.a==1:
+        if Yes.a == 1:
             print("No")
         else:
             print("Yes, but no")
-        Yes.a-=1 #Note this line
+        Yes.a -= 1 #Note this line
+
 
 Yes().yes()
 No().no()
 Yes().yes()
 No().no()
 
-print(b)
+
+
 
 
 
